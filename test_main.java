30fe910132959cc04c1eedab686d40c7e60c9bb0
MoetezMaaoui <moetezmaaoui0@gmail.com>
2024-10-02 15:02:56 +0100

    Add files via upload

--- test_main.java
+++ test_main.java
@@ -1,18 +1,24 @@
-package ex3;
+package ex4;
+
+import ex4.c_livre;
 
 public class test_main {
 
 	public static void main(String[] args) {
-		livre L1,L2,L3;
-		L1 = new livre();
-		L2 = new livre ("castro","risselte el 8ofrane");
+		c_livre L1,L2,L3;
+		L1 = new c_livre();
+		
+		L2 = new c_livre ("castro","risselte el 8ofrane",50);
 		L2.setNbPages(54);
-		L3 = new livre();
+		
+		L3 = new c_livre();
 		L3.setauteur("psycom");
 		L3.settitre("slow death");
+		L3.setprix(60);
 		L3.setNbPages(50);
+		L3.setprix(630);
 		
-		livre Tab[] = new livre[3];
+		c_livre Tab[] = new c_livre[3];
 		Tab[0] = L1;
 		Tab[1] = L2;
 		Tab[2] = L3;
@@ -26,7 +32,16 @@ public class test_main {
 		for(int i=0;i<3;i++) {
 			Tab[i].decrire();
 		}
-			
+		
+		for(int i=0;i<3;i++) {
+			System.out.println(Tab[i].getAuteur());
+		}
+		
+		for(int i=0;i<3;i++) {
+			System.out.println(Tab[i].getprix());
+		}
+		
+		
 	}
 
 }
